392c6759f7667421570dd996b1b662feb6003fef
dits1019 <jdits1019@gmail.com>
2021-07-06 09:30:10 +0900

    2일차

---
+++ lib/slide_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SlideScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('슬라이드 스크린'),
+        ),
+      ),
+    );
+  }
+}
